fdc8d920efbf1d58a8c87e47bcde3a9fa3a61092
Neo Racer <0xNeoRacer@protonmail.com>
2025-09-30 16:58:57 +0200

    virtual asset deploy

--- silo-oracles/deploy/SiloVirtualAsset8DecimalsDeploy.s.sol
+++ silo-oracles/deploy/SiloVirtualAsset8DecimalsDeploy.s.sol
@@ -27,6 +27,24 @@ import {SiloOraclesContracts} from "./SiloOraclesContracts.sol";
         --compiler-version 0.8.28 \
         --num-of-optimizations 200 \
         --watch
+
+    XDC chain deployment:
+
+    FOUNDRY_PROFILE=oracles \
+        forge script silo-oracles/deploy/SiloVirtualAsset8DecimalsDeploy.s.sol \
+        --ffi --rpc-url $RPC_XDC --legacy --broadcast --verify
+
+    FOUNDRY_PROFILE=oracles \
+        forge script silo-oracles/deploy/SiloVirtualAsset8DecimalsDeploy.s.sol \
+        --verifier-url $VERIFIER_URL_ETHERSCAN_V2 \
+        --verifier etherscan \
+        --chain 50 \
+        --ffi --rpc-url $RPC_XDC \
+        --etherscan-api-key $ETHERSCAN_API_KEY \
+        --verify \
+        --private-key $PRIVATE_KEY \
+        --legacy \
+        --resume
  */
 contract SiloVirtualAsset8DecimalsDeploy is CommonDeploy {
     function run() public returns (address asset) {

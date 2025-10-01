de3db3149156945facbb465e3f62c18888c09870
Neo Racer <0xNeoRacer@protonmail.com>
2025-10-01 16:27:18 +0200

    test silo

--- silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol
+++ silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol
@@ -6,9 +6,9 @@ import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
 import {SiloDeploy, ISiloDeployer} from "./SiloDeploy.s.sol";
 
 /*
-FOUNDRY_PROFILE=core CONFIG=Silo_wXDC_USDC HOOK_RECEIVER_OWNER=DAO \
+FOUNDRY_PROFILE=core CONFIG=Test_Silo_wXDC_USDC HOOK_RECEIVER_OWNER=DAO \
     forge script silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol \
-    --ffi --rpc-url $RPC_XDC --broadcast --verify
+    --ffi --rpc-url $RPC_XDC_APOTHEM --broadcast --verify
 
 Resume verification:
     FOUNDRY_PROFILE=core CONFIG=Silo_WAVAX_USDC HOOK_RECEIVER_OWNER=DAO \
@@ -18,24 +18,6 @@ Resume verification:
         --private-key $PRIVATE_KEY \
         --resume
 
-    XDC chain deployment:
-
-    FOUNDRY_PROFILE=core CONFIG=Silo_wXDC_USDC HOOK_RECEIVER_OWNER=DAO \
-    forge script silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol \
-    --ffi --rpc-url $RPC_XDC --legacy --broadcast \
-    -g 200 --gas-limit 500000000 --gas-price 12500000000 --verify
-
-    FOUNDRY_PROFILE=core CONFIG=Silo_wXDC_USDC HOOK_RECEIVER_OWNER=DAO \
-    forge script silo-core/deploy/silo/SiloDeployWithHookReceiverOwner.s.sol \
-    --verifier-url $VERIFIER_URL_ETHERSCAN_V2 \
-    --verifier etherscan \
-    --chain 50 \
-    --ffi --rpc-url $RPC_XDC \
-    --etherscan-api-key $ETHERSCAN_API_KEY \
-    --verify \
-    --private-key $PRIVATE_KEY \
-    --legacy \
-    --resume
  */
 contract SiloDeployWithHookReceiverOwner is SiloDeploy {
     function _getClonableHookReceiverConfig(address _implementation)

af2f4702e181738b480e90e01170037382dd837b
Neo Racer <0xNeoRacer@protonmail.com>
2026-01-07 13:02:47 +0100

    DynamicKinkModelFactory

--- silo-core/deploy/DKinkIRMFactoryDeploy.s.sol
+++ silo-core/deploy/DKinkIRMFactoryDeploy.s.sol
@@ -14,16 +14,25 @@ import {DynamicKinkModel} from "silo-core/contracts/interestRateModel/kink/Dynam
 /*
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/DKinkIRMFactoryDeploy.s.sol:DKinkIRMFactoryDeploy \
-        --ffi --rpc-url $RPC_MAINNET --broadcast --verify
+        --ffi --rpc-url $RPC_INJECTIVE --broadcast --slow --verify
 
     Resume verification:
     FOUNDRY_PROFILE=core \
         forge script silo-core/deploy/DKinkIRMFactoryDeploy.s.sol:DKinkIRMFactoryDeploy \
-        --ffi --rpc-url $RPC_INK \
+        --ffi --rpc-url $RPC_INJECTIVE \
         --verify \
-        --verifier blockscout --verifier-url $VERIFIER_URL_INK \
+        --verifier blockscout \
+        --verifier-url $VERIFIER_URL_INJECTIVE \
         --private-key $PRIVATE_KEY \
         --resume
+
+    FOUNDRY_PROFILE=core forge verify-contract 0x80e3f3d136f4B3b8A1f0693d3290184445cdee8E \
+         silo-core/contracts/interestRateModel/kink/DynamicKinkModelFactory.sol:DynamicKinkModelFactory \
+        --verifier blockscout \
+        --verifier-url $VERIFIER_URL_INJECTIVE \
+        --compiler-version 0.8.28 \
+        --num-of-optimizations 200 \
+        --watch
  */
 contract DKinkIRMFactoryDeploy is CommonDeploy {
     function run() public returns (IDynamicKinkModelFactory irmFactory) {

ca384e0061e65587f8515bc6a7ca9d364411f841
Neo Racer <0xNeoRacer@protonmail.com>
2026-02-16 18:55:22 +0100

    base

--- silo-core/deploy/_CommonDeploy.sol
+++ silo-core/deploy/_CommonDeploy.sol
@@ -44,6 +44,7 @@ contract CommonDeploy is Deployer {
         if (chainId == ChainsLib.INK_CHAIN_ID) return AddrLib.getAddress(AddrKey.WETH);
         if (chainId == ChainsLib.BNB_CHAIN_ID) return AddrLib.getAddress(AddrKey.WBNB);
         if (chainId == ChainsLib.OKX_CHAIN_ID) return AddrLib.getAddress(AddrKey.WOKB);
+        if (chainId == ChainsLib.BASE_CHAIN_ID) return AddrLib.getAddress(AddrKey.WETH);
 
         revert(string.concat("can not find native token for ", ChainsLib.chainAlias()));
     }

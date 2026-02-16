3d1c29977878123ee751a2afc3589461b6e4332f
Neo Racer <0xNeoRacer@protonmail.com>
2026-02-16 16:39:37 +0100

    silo id

--- silo-core/contracts/SiloFactory.sol
+++ silo-core/contracts/SiloFactory.sol
@@ -52,7 +52,6 @@ contract SiloFactory is ISiloFactory, ERC721, Ownable2Step, IVersioned {
         ERC721("Silo Finance Fee Receiver", "feeSILO")
         Ownable(msg.sender)
     {
-        // start IDs from 100
         _siloId = 100;
 
         baseURI = "https://v2.app.silo.finance/markets/";

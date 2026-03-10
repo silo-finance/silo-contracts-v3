6653a83809c28e7a8ffd4820bc2e64daa636a4f2
Neo Racer <0xNeoRacer@protonmail.com>
2026-03-10 18:12:13 +0100

    price

--- silo-core/deploy/silo/verifier/Logger.sol
+++ silo-core/deploy/silo/verifier/Logger.sol
@@ -149,8 +149,9 @@ contract Logger is Test {
         if (configData0.solvencyOracle != address(0)) {
             _logOracle(ISiloOracle(configData0.solvencyOracle), configData0.token);
         } else {
-            console2.log("\n\tOracle is not set for silo0, price is: 1.0e18\n", DELIMITER);
-
+            uint256 tokenDecimals = Utils.tryGetTokenDecimals(configData0.token);
+            string memory price = PriceFormatter.formatPriceInE18(10 ** tokenDecimals);
+            console2.log("\n\tOracle is not set for silo0, price will be: ", price, "\n", DELIMITER);
         }
 
         if (configData0.maxLtvOracle != configData0.solvencyOracle && configData0.maxLtvOracle != address(0)) {
@@ -160,7 +161,9 @@ contract Logger is Test {
         if (configData1.solvencyOracle != address(0)) {
             _logOracle(ISiloOracle(configData1.solvencyOracle), configData1.token);
         } else {
-            console2.log("\n\tOracle is not set for silo1, price is: 1.0e18\n", DELIMITER);
+            uint256 tokenDecimals = Utils.tryGetTokenDecimals(configData1.token);
+            string memory price = PriceFormatter.formatPriceInE18(10 ** tokenDecimals);
+            console2.log("\n\tOracle is not set for silo1, price will be: ", price, "\n", DELIMITER);
         }
 
         if (configData1.maxLtvOracle != configData1.solvencyOracle && configData1.maxLtvOracle != address(0)) {

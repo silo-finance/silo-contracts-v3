4a9ff39c4852d58f1ac362a7c713f09260e8bb2a
Neo Racer <0xNeoRacer@protonmail.com>
2026-03-24 15:55:03 +0100

    linter

--- silo-vaults/contracts/incentives/claiming-logics/SiloIncentivesControllerCL.sol
+++ silo-vaults/contracts/incentives/claiming-logics/SiloIncentivesControllerCL.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.28;
 
-import {SafeCast} from "openzeppelin5/utils/math/SafeCast.sol";
-
 import {
     ISiloIncentivesController,
     IDistributionManager

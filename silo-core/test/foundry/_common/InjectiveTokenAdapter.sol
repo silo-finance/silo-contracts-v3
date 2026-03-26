9d802d54917973f517a64440339a47a5809d6e44
Neo Racer <0xNeoRacer@protonmail.com>
2026-03-26 17:49:52 +0100

    .

--- silo-core/test/foundry/_common/InjectiveTokenAdapter.sol
+++ silo-core/test/foundry/_common/InjectiveTokenAdapter.sol
@@ -20,7 +20,7 @@ contract InjectiveTokenAdapter {
         require(d != 0, "decimals not set, check InjectiveWorkaround");
     }
 
-    function symbol() external view returns (string memorys s) {
+    function symbol() external view returns (string memory s) {
         (s,,) = BANK_MODULE.metadata(TOKEN);
     }
 

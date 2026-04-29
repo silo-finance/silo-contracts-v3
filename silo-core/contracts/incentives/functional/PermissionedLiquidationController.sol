a2bda165271642701b4cdc9a2cb5c6d962040a9a
Neo Racer <0xNeoRacer@protonmail.com>
2026-04-29 12:55:46 +0200

    interface

--- silo-core/contracts/incentives/functional/PermissionedLiquidationController.sol
+++ silo-core/contracts/incentives/functional/PermissionedLiquidationController.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.28;
 
+import {console2} from "forge-std/console2.sol";
+
 import {SafeERC20} from "openzeppelin5/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
 import {IERC4626} from "openzeppelin5/interfaces/IERC4626.sol";
@@ -10,10 +12,17 @@ import {ISiloConfig} from "silo-core/contracts/interfaces/ISiloConfig.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
 import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
 import {IPartialLiquidation} from "silo-core/contracts/interfaces/IPartialLiquidation.sol";
+import {
+    IPermissionedLiquidationController
+} from "silo-core/contracts/interfaces/IPermissionedLiquidationController.sol";
 import {Whitelist} from "silo-core/contracts/hooks/_common/Whitelist.sol";
 import {BaseHookReceiver} from "silo-core/contracts/hooks/_common/BaseHookReceiver.sol";
 
-contract PermissionedLiquidationController is SiloIncentivesControllerCompatible, Whitelist {
+contract PermissionedLiquidationController is
+    IPermissionedLiquidationController,
+    SiloIncentivesControllerCompatible,
+    Whitelist
+{
     using SafeERC20 for IERC20;
 
     address public immutable HOOK_RECEIVER;
@@ -25,34 +34,42 @@ contract PermissionedLiquidationController is SiloIncentivesControllerCompatible
 
     bool private transient _liquidationAllowed;
 
-    error LiquidationNotAllowed();
-    error STokenNotSupported();
-
     /// @param _owner owner of the contract
     /// @param _notifier for Silo it should be hook address
-    /// @param _shareTokenAddress any share token address, it will be used to get hook receiver and silo
+    /// @param _shareTokenAddress protected or collateral share token address
     constructor(address _owner, address _notifier, address _shareTokenAddress)
         SiloIncentivesControllerCompatible(_owner, _notifier, _shareTokenAddress)
     {
         __Whitelist_init(_owner);
 
         HOOK_RECEIVER = IShareToken(_shareTokenAddress).hookReceiver();
+        require(address(HOOK_RECEIVER) == _notifier, InvalidHookReceiver());
 
         address collateralSilo = address(IShareToken(_shareTokenAddress).silo());
         ISiloConfig siloConfig = IShareToken(_shareTokenAddress).siloConfig();
         (address silo0, address silo1) = siloConfig.getSilos();
         DEBT_SILO = silo0 == collateralSilo ? silo1 : silo0;
+
+        ISiloConfig.ConfigData memory collateralConfig = siloConfig.getConfig(collateralSilo);
+        require(collateralConfig.lt != 0, NotCollateralSilo());
+
+        require(
+            collateralConfig.collateralShareToken == _shareTokenAddress
+                || collateralConfig.protectedShareToken == _shareTokenAddress,
+            NotCollateralShareToken()
+        );
+
         DEBT_ASSET = IERC20(siloConfig.getAssetForSilo(DEBT_SILO));
-        COLLATERAL_ASSET = IERC20(siloConfig.getAssetForSilo(collateralSilo));
+        COLLATERAL_ASSET = IERC20(collateralConfig.token);
 
         DEBT_ASSET.approve(HOOK_RECEIVER, type(uint256).max);
     }
 
+    /// @inheritdoc IPermissionedLiquidationController
     function setEnabled(bool _enabled) external onlyOwner {
         enabled = _enabled;
     }
 
-    /// @dev this incentive controller needs to be set for protected and collateral
     function afterTokenTransfer(
         address _sender,
         uint256 /*_senderBalance*/,
@@ -69,15 +86,25 @@ contract PermissionedLiquidationController is SiloIncentivesControllerCompatible
 
         // is this liquidation?
         // After transferring collateral, the user will always be insolvent.
-        (address silo,) = BaseHookReceiver(msg.sender).siloConfig().getSilos();
-        bool isLiquidation = !ISilo(silo).isSolvent(_sender);
+        (address anySilo,) = BaseHookReceiver(msg.sender).siloConfig().getSilos();
+        bool isLiquidation = !ISilo(anySilo).isSolvent(_sender);
+
+        console2.log("isLiquidation", isLiquidation);
+        console2.log("liquidationAllowed", _liquidationAllowed);
+        console2.log("reverting?", isLiquidation && !_liquidationAllowed);
 
         if (isLiquidation && !_liquidationAllowed) revert LiquidationNotAllowed();
     }
 
-    // TODO check allowances 
+    /// @inheritdoc IPermissionedLiquidationController
+    function allowMeToLiquidate() external virtual onlyAllowed {
+        _liquidationAllowed = true;
+    }
+
+    // TODO check allowances
     // TODO should we adapt liquidation helper?
     // TODO should we adapt manual liquidation?
+    /// @inheritdoc IPermissionedLiquidationController
     function liquidationCall( // solhint-disable-line function-max-lines, code-complexity
         address _collateralAsset,
         address _debtAsset,
---
+++ silo-core/contracts/interfaces/IPermissionedLiquidationController.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.28;
+
+import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
+
+interface IPermissionedLiquidationController {
+    error LiquidationNotAllowed();
+    error STokenNotSupported();
+    error InvalidHookReceiver();
+    error NotCollateralShareToken();
+    error NotCollateralSilo();
+
+    function HOOK_RECEIVER() external view returns (address);
+
+    function DEBT_ASSET() external view returns (IERC20);
+
+    function DEBT_SILO() external view returns (address);
+
+    function COLLATERAL_ASSET() external view returns (IERC20);
+
+    function enabled() external view returns (bool);
+
+    function setEnabled(bool _enabled) external;
+
+    function allowMeToLiquidate() external;
+
+    function liquidationCall(
+        address _collateralAsset,
+        address _debtAsset,
+        address _borrower,
+        uint256 _maxDebtToCover,
+        bool _receiveSToken
+    ) external returns (uint256 withdrawCollateral, uint256 repayDebtAssets);
+}

85f7530f7ce9b21d401542c0adcb0ea3a331cefc
Neo Racer <0xNeoRacer@protonmail.com>
2026-05-06 11:32:46 +0200

    emit

--- silo-core/contracts/incentives/functional/PermissionedLiquidationController.sol
+++ silo-core/contracts/incentives/functional/PermissionedLiquidationController.sol
@@ -77,7 +77,10 @@ contract PermissionedLiquidationController is
         require(_permisionedData.pauseTokenTransfer != _pauseTokenTransfer, PauseTokenTransferAlreadySet());
 
         _permisionedData.pauseTokenTransfer = _pauseTokenTransfer;
-        if (_pauseTokenTransfer) _permisionedData.enabled = true;
+        if (_pauseTokenTransfer) {
+            _permisionedData.enabled = true;
+            emit EnabledChanged(true);
+        }
 
         emit PauseTokenTransferChanged(_pauseTokenTransfer);
     }

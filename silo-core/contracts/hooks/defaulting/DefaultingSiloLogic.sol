d9e63382182f88eb3bf7473a9e404596dfeae9d3
Neo Racer <0xNeoRacer@protonmail.com>
2026-06-09 13:43:45 +0200

    path stopped working?

--- silo-core/contracts/hooks/defaulting/DefaultingSiloLogic.sol
+++ silo-core/contracts/hooks/defaulting/DefaultingSiloLogic.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.28;
 
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
-import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import {Math} from "openzeppelin5/utils/math/Math.sol";
 import {SafeCast} from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
 
 import {SiloStorageLib} from "silo-core/contracts/lib/SiloStorageLib.sol";

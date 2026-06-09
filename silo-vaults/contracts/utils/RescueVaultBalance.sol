8780c690236860b8e3fd76efa459a07a29b4257e
Neo Racer <0xNeoRacer@protonmail.com>
2026-06-09 13:54:38 +0200

    linter

--- silo-vaults/contracts/utils/RescueVaultBalance.sol
+++ silo-vaults/contracts/utils/RescueVaultBalance.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {IERC20} from "openzeppelin5/token/ERC20/IERC20.sol";
 import {IERC4626} from "openzeppelin5/interfaces/IERC4626.sol";
 
 import {IIncentivesClaimingLogic} from "../interfaces/IIncentivesClaimingLogic.sol";

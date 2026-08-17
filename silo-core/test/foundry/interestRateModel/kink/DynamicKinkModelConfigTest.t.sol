928b1f82cfb1aa69b05f13f15da4d81fba452f78
Neo Racer <0xNeoRacer@protonmail.com>
2026-08-17 12:23:52 +0200

    fix test

--- silo-core/test/foundry/interestRateModel/kink/DynamicKinkModelConfigTest.t.sol
+++ silo-core/test/foundry/interestRateModel/kink/DynamicKinkModelConfigTest.t.sol
@@ -11,12 +11,18 @@ contract DynamicKinkModelConfigTest is KinkCommonTest {
     /*
     FOUNDRY_PROFILE=core_test forge test --mt test_kink_config_getConfig -vv
     */
-    function test_kink_config_getConfig(IDynamicKinkModel.Config memory _config) public {
-        config = new DynamicKinkModelConfig(_config);
+    function test_kink_config_getConfig(
+        IDynamicKinkModel.Config memory _config,
+        IDynamicKinkModel.ImmutableConfig memory _immutableConfig
+    ) public {
+        config = new DynamicKinkModelConfig(_config, _immutableConfig);
 
         bytes32 hashIn = _hashConfig(_config);
-        bytes32 hashOut = _hashConfig(config.getConfig());
+        (IDynamicKinkModel.Config memory cfg, IDynamicKinkModel.ImmutableConfig memory imm) = config.getConfig();
+        bytes32 hashCfgOut = _hashConfig(cfg);
+        bytes32 hashImmOut = _hashImmutableConfig(imm);
 
-        assertEq(hashIn, hashOut, "hashIn != hashOut");
+        assertEq(hashIn, hashCfgOut, "hashIn != hashCfgOut");
+        assertEq(hashIn, hashImmOut, "hashIn != hashImmOut");
     }
 }
